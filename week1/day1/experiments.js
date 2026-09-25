7575dd5a1b420e4e9d10e0fb1ad73a04d16cea16
dev__abby <abigailadeboga@gmail.com>
2026-09-25 16:06:07 +0100

    Create experiments.js

---
+++ week1/day1/experiments.js
@@ -0,0 +1,53 @@
+console.log("5" + 3);
+// "53"
+console.log("5" - 3);
+// 2
+console.log("5" * "2");
+// "55"
+console.log(5 + true)
+// Error
+console.log(5 + null)
+// Error
+console.log(5 + undefined)
+// Error
+console.log("" + 0)
+// "0"
+console.log([] + [])
+// []
+console.log({} + {})
+// {}
+
+console.log(null == undefined)
+// true
+console.log(null === undefined)
+// false
+console.log(0 == "")
+// true
+console.log(0 === "")
+// false
+console.log(0 == false)
+// true
+console.log("0" == false)
+// false
+console.log(NaN == NaN)
+// false
+
+console.log(typeof 42)
+// int
+console.log(typeof "hello")
+// str
+console.log(typeof true)
+// bool
+console.log(typeof undefined)
+// 
+console.log(typeof null)
+// 
+console.log(typeof [])
+// Object
+
+console.log(0.1 + 0.2)
+// 0.3
+console.log(0.1 + 0.2 === 0.3)
+// false
+
+
